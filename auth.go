ac609303ea9436ba6c193fb78f42a1118b7956a4
Junx wan <hugh8845sam@gmail.com>
2018-09-09 14:07:18 +0800

    feat: 新增註冊功能

--- auth.go
+++ auth.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type user struct {
+	Username string `form:"username" binding:"required,email"`
+	Password string `form:"password" binding:"required"`
+}
+
+var account = []user{}
+
 func showLogin(c *gin.Context) {
 	c.HTML(http.StatusOK, "login.html", gin.H{
 		"title": "Login",
@@ -14,5 +21,28 @@ func showLogin(c *gin.Context) {
 func showRegister(c *gin.Context) {
 	c.HTML(http.StatusOK, "register.html", gin.H{
 		"title": "Register",
+	})
+}
+
+func register(c *gin.Context) {
+	var form user
+	message := ""
+
+	username, _ := c.GetPostForm("username")
+	password, _ := c.GetPostForm("password")
+
+	if err := c.ShouldBind(&form); err == nil {
+		account = append(account, user{
+			Username: username,
+			Password: password,
+		})
+
+		message = "恭喜你註冊成功，請前往登入頁做登入"
+	} else {
+		message = "帳號密碼輸入有誤請重新填寫"
+	}
+
+	c.HTML(http.StatusOK, "result.html", gin.H{
+		"message": message,
 	})
 }
--- route.go
+++ route.go
@@ -4,4 +4,5 @@ func webRoute() {
 	router.GET("/", showIndex)
 	router.GET("/login", showLogin)
 	router.GET("/register", showRegister)
+	router.POST("/register", register)
 }
