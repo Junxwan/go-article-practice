22ad2abe43ab65e61746844496cdd2a41aee926e
Junx wan <hugh8845sam@gmail.com>
2018-09-09 13:10:03 +0800

    chore: 初始專案

---
+++ article.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
+
+type article struct {
+	ID        int
+	Title     string
+	Content   string
+	CreatedAt string
+}
+
+var articleList = []article{
+	article{ID: 1, Title: "Title 1", Content: "body 1", CreatedAt: now()},
+}
+
+func showIndex(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"title":   "go-article-practice",
+		"article": articleList,
+	})
+}
+
+func now() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
---
+++ route.go
@@ -0,0 +1,5 @@
+package main
+
+func webRoute() {
+	router.GET("/", showIndex)
+}
